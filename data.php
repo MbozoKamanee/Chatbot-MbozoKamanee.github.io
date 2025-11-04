f82ea730d1b7fd2e9d73e0c35f8d1a43f453c304
Firman Kamane <firmansyahlasinta94@gmail.com>
2025-11-04 21:09:03 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,50 @@
+<?php
+$konten = [
+  "beranda" => [
+    [
+      "judul" => "Bupati Bima",
+      "deskripsi" => "Bupati dan wakil Bupati Bima saat ini",
+      "gambar" => "asset/bupati.jpg"
+    ],
+    [
+      "judul" => "Rimpu Bima",
+      "deskripsi" => "Pakaian adat wanita Bima yang mencerminkan identitas dan nilai-nilai budaya.",
+      "gambar" => "asset/rimpu.jpg"
+    ],
+    [
+      "judul" => "Nelayan Bima",
+      "deskripsi" => "Kehidupan nelayan di pesisir pantai Sangiang 'Bima'.",
+      "gambar" => "asset/nelayan.jpg"
+    ],
+    [
+      "judul" => "Petani Bima",
+      "deskripsi" => "Kegiatan petani wijen di bawah kaki gunung Sangiang 'Bima'.",
+      "gambar" => "asset/petani.jpg"
+    ]
+  ],
+  "sejarah" => [
+    [
+      "judul" => "Asal Usul Kerajaan Bima",
+      "deskripsi" => "Kerajaan Bima didirikan pada abad ke-14 oleh para bangsawan dari Dompu dan Majapahit.",
+      "gambar" => "asset/sejarah1.jpg"
+    ],
+    [
+      "judul" => "Islamisasi Bima",
+      "deskripsi" => "Proses penyebaran Islam di Bima dimulai pada abad ke-17 dan berpengaruh besar pada budaya lokal.",
+      "gambar" => "asset/sejarah2.jpg"
+    ]
+  ],
+  "pariwisata" => [
+    [
+      "judul" => "Pantai Wane",
+      "deskripsi" => "Pantai indah di selatan Bima dengan ombak yang cocok untuk berselancar.",
+      "gambar" => "asset/pantai-wane.jpg"
+    ],
+    [
+      "judul" => "Gunung Tambora",
+      "deskripsi" => "Gunung legendaris dengan sejarah letusan dahsyat tahun 1815 dan pemandangan menakjubkan.",
+      "gambar" => "asset/tambora.jpg"
+    ]
+  ]
+];
+?>
